382d79f65136b34ac943758f092dcf9c8a3b36a2
Knapek Laptop <knapekk@gmail.com>
2021-06-12 22:08:58 +0200

    Logo appear at navbar after scrolldown.

--- script/sctipt.js
+++ script/sctipt.js
@@ -3,12 +3,22 @@ console.log("siemanko");
 
 //Navbar shadow event listener 
 document.addEventListener('scroll', function(){
-   var element = document.getElementsByTagName('nav');
+   var navNode = document.getElementsByTagName('nav');
+   var logoImgNode = document.createElement('img');
+   var navLogoContainerNode = document.getElementById('nav-logo-container-id');
 
    if(document.documentElement.scrollTop != 0){
-      element[0].classList.add('navShadowScroll');
+
+      navNode[0].classList.add('navShadowScroll');
+
+      if(document.getElementById('nav-logo-container-id').children.length == 0){
+         logoImgNode.setAttribute('src', './img/logo.png');
+         document.getElementById('nav-logo-container-id').append(logoImgNode);
+      }
    }else{
-      element[0].classList.remove('navShadowScroll'); 
+      navNode[0].classList.remove('navShadowScroll'); 
+      navLogoContainerNode.removeChild(navLogoContainerNode.firstChild);
+      
    };
 });
 
