730cf549d33b650709548f70e5fb0d07e36c4919
Knapek Laptop <knapekk@gmail.com>
2021-06-12 19:06:51 +0200

    Navbar bottom shadoe effect has been implemented. Logo container has been created.

--- script/sctipt.js
+++ script/sctipt.js
@@ -1,1 +1,15 @@
 console.log("siemanko");
+
+
+//Navbar shadow event listener 
+document.addEventListener('scroll', function(){
+   var element = document.getElementsByTagName('nav');
+
+   if(document.documentElement.scrollTop != 0){
+      element[0].classList.add('navShadowScroll');
+   }else{
+      element[0].classList.remove('navShadowScroll'); 
+   };
+});
+
+
