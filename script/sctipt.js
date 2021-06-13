c0d73c4f3de8fb035db927a7cd56c790f175ae85
Knapek Laptop <knapekk@gmail.com>
2021-06-13 13:51:00 +0200

    fixing logo apperance at navbar. comments added.

--- script/sctipt.js
+++ script/sctipt.js
@@ -1,24 +1,25 @@
-console.log("siemanko");
-
-
 //Navbar shadow event listener 
-document.addEventListener('scroll', function(){
+document.addEventListener('scroll', function()
+{
    var navNode = document.getElementsByTagName('nav');
    var logoImgNode = document.createElement('img');
-   var navLogoContainerNode = document.getElementById('nav-logo-container-id');
+   var navbarLogoContainerDivNode = document.getElementById('nav-logo-container-id');
 
-   if(document.documentElement.scrollTop != 0){
+if(document.documentElement.scrollTop > 60) //ture if pahe has been scrolled 60 px from the top.
+   {
 
-      navNode[0].classList.add('navShadowScroll');
+      navNode[0].classList.add('navShadowScroll'); //adding class navShadowScroll when page is scrolled down.
 
-      if(document.getElementById('nav-logo-container-id').children.length == 0){
-         logoImgNode.setAttribute('src', './img/logo.png');
-         document.getElementById('nav-logo-container-id').append(logoImgNode);
+      if(document.getElementById('nav-logo-container-id').children.length == 0) //adding logo to navbar when page is scrolled down.
+      {
+         logoImgNode.setAttribute('src', './img/logo_navbar.png'); //setting atribute with link to logo at img node.
+         document.getElementById('nav-logo-container-id').append(logoImgNode); //appending img node to dedicated div container.
       }
-   }else{
-      navNode[0].classList.remove('navShadowScroll'); 
-      navLogoContainerNode.removeChild(navLogoContainerNode.firstChild);
-      
+   }
+   else
+   {
+      navNode[0].classList.remove('navShadowScroll'); //removing class navShadowScroll when page is at the top.
+      navbarLogoContainerDivNode.removeChild(navbarLogoContainerDivNode.firstChild); //removing img node from dedicated div container when page is at the top.
    };
 });
 
