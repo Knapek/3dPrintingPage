0517882755589c165029fce2662b5f3732328db8
Knapek Laptop <knapekk@gmail.com>
2021-06-17 13:19:56 +0200

    Fade of logo, first shape. Scroll event listener

--- script/sctipt.js
+++ script/sctipt.js
@@ -4,23 +4,29 @@ document.addEventListener('scroll', function()
    var navNode = document.getElementsByTagName('nav');
    var logoImgNode = document.createElement('img');
    var navbarLogoContainerDivNode = document.getElementById('nav-logo-container-id');
+   var navbarLogoContainerDivNodeStyle = window.getComputedStyle(navbarLogoContainerDivNode);
+   var navbarLogoContainerDivNodeStyleOpacity = navbarLogoContainerDivNodeStyle.getPropertyValue('opacity');
 
 if(document.documentElement.scrollTop > 30) //ture if pahe has been scrolled 60 px from the top.
    {
-
-      navNode[0].classList.add('shadow-lg'); //adding class navShadowScroll when page is scrolled down.
-
-      if(document.getElementById('nav-logo-container-id').children.length == 0) //adding logo to navbar when page is scrolled down.
-      {
-         logoImgNode.setAttribute('src', './img/logo_navbar.png'); //setting atribute with link to logo at img node.
-         document.getElementById('nav-logo-container-id').append(logoImgNode); //appending img node to dedicated div container.
+      navNode[0].classList.add('shadow-lg'); //removing class navShadowScroll when page is at the top.   
+      if(navbarLogoContainerDivNodeStyleOpacity == 0){
+         navbarLogoContainerDivNode.classList.toggle('fade');
+         
       }
    }
    else
    {
       navNode[0].classList.remove('shadow-lg'); //removing class navShadowScroll when page is at the top.
-      navbarLogoContainerDivNode.removeChild(navbarLogoContainerDivNode.firstChild); //removing img node from dedicated div container when page is at the top.
+      // navbarLogoContainerDivNode.classList.toggle('fade');
+      if(navbarLogoContainerDivNodeStyleOpacity == 1){
+         navbarLogoContainerDivNode.classList.toggle('fade');
+      }
    };
+
+
+
 });
 
+ 
 
