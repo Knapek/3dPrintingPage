88afc9edcb8bf8a87c65e452e610a898e1432e4f
Knapek Laptop <knapekk@gmail.com>
2021-06-18 10:58:10 +0200

    fade of logo updated, but not completely fixed.

--- script/sctipt.js
+++ script/sctipt.js
@@ -1,32 +1,35 @@
-//Navbar shadow event listener 
-document.addEventListener('scroll', function()
-{
+
+function fadeEffect() {
    var navNode = document.getElementsByTagName('nav');
-   var logoImgNode = document.createElement('img');
    var navbarLogoContainerDivNode = document.getElementById('nav-logo-container-id');
    var navbarLogoContainerDivNodeStyle = window.getComputedStyle(navbarLogoContainerDivNode);
    var navbarLogoContainerDivNodeStyleOpacity = navbarLogoContainerDivNodeStyle.getPropertyValue('opacity');
 
-if(document.documentElement.scrollTop > 30) //ture if pahe has been scrolled 60 px from the top.
+   
+   if(document.documentElement.scrollTop > 100) //ture if pahe has been scrolled 100 px from the top.
    {
-      navNode[0].classList.add('shadow-lg'); //removing class navShadowScroll when page is at the top.   
-      if(navbarLogoContainerDivNodeStyleOpacity == 0){
-         navbarLogoContainerDivNode.classList.toggle('fade');
-         
+      navNode[0].classList.add('shadow-lg'); //removing class navShadowScroll when page is at the top.  
+
+      if(navbarLogoContainerDivNodeStyleOpacity == 0)
+      {
+         navbarLogoContainerDivNode.classList.toggle('fade'); //changing of the opacity
       }
    }
    else
    {
       navNode[0].classList.remove('shadow-lg'); //removing class navShadowScroll when page is at the top.
-      // navbarLogoContainerDivNode.classList.toggle('fade');
-      if(navbarLogoContainerDivNodeStyleOpacity == 1){
-         navbarLogoContainerDivNode.classList.toggle('fade');
+      
+      if(navbarLogoContainerDivNodeStyleOpacity == 1)
+      {
+         navbarLogoContainerDivNode.classList.toggle('fade');// reverse change of the opacity
       }
    };
+   return navbarLogoContainerDivNodeStyleOpacity;
+}
 
-
-
+//Navbar shadow event listener 
+document.addEventListener('scroll',function(){
+   fadeEffect();
 });
 
- 
 
